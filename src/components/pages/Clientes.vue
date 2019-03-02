cd943189f9e74afca0ae115bd3f2b7d0bf0009b6
Rafael Cavalcante Lima <rafaelc.lima@outlook.com>
2019-03-02 01:05:20 -0300

    feat: include section-clients and adjustments to the previous layout.

---
+++ src/components/pages/Clientes.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-layout row wrap
+    id="oct-clientes"
+    class="text-xs-center grey lighten-4"
+    >
+    <v-container>
+            <h1 class="display-2 primary--text oct-mg-top-2x">Clientes</h1>
+    </v-container>
+    <v-flex md2 xs6 offset-md4>
+        <v-img src="http://i65.tinypic.com/29db8nq.png" 
+            aspect-ratio="2" width="90%" height="auto" contain></v-img>
+    </v-flex>
+    <v-flex md2 xs6>
+        <v-img src="http://i66.tinypic.com/kamzkn.png" 
+            aspect-ratio="2" width="90%" height="auto" contain></v-img>
+    </v-flex>
+    </v-layout>
+</template>
+
+<style>
+    #oct-clientes{
+        margin-top: 64px;
+    }
+</style>
+
+<script>
+export default {
+    
+}
+</script>
--- src/components/pages/OqueFazemos.vue
+++ src/components/pages/OqueFazemos.vue
@@ -2,22 +2,20 @@
     <v-layout fluid="true"
     id="OqueFazemos"
     class="secondary white--text oct-OqueFazemos-height"
+    row wrap
     >
 
-    <v-container grid-list-lg
-    >
-        <v-layout row wrap
-        >
-            <v-flex xs12
-            align-start="true"
-            >
-                <h1 class="display-2 text-xs-center">O que Fazemos</h1>
+        <v-flex xs12 md6> 
+                <v-parallax src="http://i68.tinypic.com/2hg5q38.jpg"></v-parallax>
+        </v-flex>
+        <v-flex xs12 md6> 
+            <v-container grid-list-lg>
+                <h1 class="display-2 text-md-center oct-mg-top-2x">O que fazemos</h1>
                 
-                <p class="subheading oct-mg-top">A <strong>Octotech</strong> é uma start-up especializada em inovação, que trabalha imersa nos objetivos de negócios de nossos clientes e nas necessidades de seus usuários, para isso utilizamos uma visão centrada no humano e o poder transformador do design. Para facilitar e impulsionar ideias e novos modelos de negócios utilizamos o Design Thinking e métodos como o Double Diamond, que vai além de uma simples ferramenta para designers, pois pode ser aplicado para qualquer problema que requer uma solução, desenvolvendo experiências únicas e que trazem equilíbrio entre negócios e pessoas.</p>
-
-            </v-flex>
-        </v-layout>
-    </v-container>
+                <p class="subheading oct-mg-top">A <strong class="accent--text">Octotech</strong> é uma start-up especializada em inovação, que trabalha imersa nos objetivos de negócios de nossos clientes e nas necessidades de seus usuários, para isso utilizamos uma visão centrada no humano e o poder transformador do design. Para facilitar e impulsionar ideias e novos modelos de negócios utilizamos o Design Thinking e métodos como o Double Diamond, que vai além de uma simples ferramenta para designers, pois pode ser aplicado para qualquer problema que requer uma solução, desenvolvendo experiências únicas e que trazem equilíbrio entre negócios e pessoas.</p>
+                <br>
+            </v-container>
+        </v-flex>
     </v-layout>
 </template>
 
